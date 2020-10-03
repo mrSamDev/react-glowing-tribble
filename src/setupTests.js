59a54499920f6df9264a86d96ebc71a2c816e964
Sijo sam <sijo@7thpillar.com>
2020-10-03 18:12:02 +0530

    react-create-app on Heroku

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,8 +4,13 @@ import { configure } from "enzyme";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import App from "./App";
+import Auth from "./components/page/Auth";
 configure({ adapter: new Adapter() });
 
 it("renders without crashing", () => {
   shallow(<App />);
+});
+
+it("renders auth without crashing", () => {
+  shallow(<Auth />);
 });
